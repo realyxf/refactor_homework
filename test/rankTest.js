2b50eca99e47e568605c41d1764ac94d3cef2598
你的用户名 <997915421@qq.com>
2020-08-27 22:35:38 +0800

    [Refactoring homework][Fennie] test: rank case 1. voyage zone is east-indies and length is 4.

---
+++ test/rankTest.js
@@ -0,0 +1,21 @@
+const rankTest = require('ava');
+const { voyageRisk } = require('../src/rank');
+
+rankTest('foo', t => {
+  t.pass();
+});
+
+rankTest('bar', async t => {
+  const bar = Promise.resolve('bar');
+  t.is(await bar, 'bar');
+});
+
+rankTest('rank case 1. voyage zone is east-indies and length is 4.', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 4,
+  };
+  const result = voyageRisk(voyage)
+
+  t.is(result, 1);
+})

c168c3731088d03bcb33f73d553291834b2f3800
你的用户名 <997915421@qq.com>
2020-08-27 22:38:43 +0800

    [Refactoring homework][Fennie] test: rank case 2. voyage zone is east-indies and length is 5.

--- test/rankTest.js
+++ test/rankTest.js
@@ -19,3 +19,13 @@ rankTest('rank case 1. voyage zone is east-indies and length is 4.', t => {
 
   t.is(result, 1);
 })
+
+rankTest('rank case 2. voyage zone is east-indies and length is 5.', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 5,
+  };
+  const result = voyageRisk(voyage)
+
+  t.is(result, 3);
+})
